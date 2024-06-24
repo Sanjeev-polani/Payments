08619fe44b2b695cba4ecf7dd186afc95739abb7
sanje <sanje@SanjeevKumarP>
2024-06-24 11:18:37 +0530

    First Commit

---
+++ src/Day1/Datavariables.java
@@ -0,0 +1,43 @@
+package Day1;
+
+public class Datavariables {
+
+	public static void main(String[] args) 
+	{
+		int a;  	//declaration
+		a=100;		//assignment
+		
+		System.out.println(a);
+		
+		a =200;
+		System.out.println(a);
+		
+		
+		//Approach 1
+		int e =500;
+		int b =600;
+		int c =700;
+		
+		//Approach 2
+		int h,i; 		//declaration
+		
+		
+		char grade = 'A';
+		System .out.println(grade);
+		byte by =125;
+		System .out.println(by);
+		
+		short sh = 3535;
+		System .out.println(sh);
+		
+		long   l = 123456789L;  //literal is needed L
+		System .out.println(l);
+		
+		float item =15.5F;  //literal is needed F
+		System .out.println(item);
+		
+		double dbl = 1234.489938938;
+		System .out.println(dbl);
+	}
+
+}
---
+++ src/Day1/Simple.java
@@ -0,0 +1,20 @@
+package Day1;
+
+public class Simple           //access modifier = public 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		int a,b,c;
+		a=10;
+		b=30;
+		c=a+b;
+		System.out.println("Addition of the values a+b is : "+c);
+		//System.out.println(c);
+		
+
+	}
+
+}
